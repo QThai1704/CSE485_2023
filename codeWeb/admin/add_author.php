b1368c4602773cce2eff34076cfda529444634d0
Long Nhật <doannhat802@gmail.com>
2023-02-25 22:43:12 +0700

    update author and add_author

--- codeWeb/admin/add_author.php
+++ codeWeb/admin/add_author.php
@@ -1,7 +1,6 @@
 <?php
 require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +8,7 @@ require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Author</title>
+    <title>Music for Life</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
@@ -46,56 +45,47 @@ require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
                 </div>
             </div>
         </nav>
+
     </header>
     <main class="container mt-5 mb-5">
-        <div>
-            <div>
-                <button type="button" class="btn btn-primary">
-                    <a href="add_author.php?id=1" style="text-decoration: none; color:aliceblue">Thêm mới</a>
-                </button>
-            </div>
-            <div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Mã tác giả</th>
-                            <th>Tên tác giả</th>
-                            <th>Hình ảnh tác giả</th>
-                            <th>Sửa</th>
-                            <th>Xóa</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $sql = 'select * from tacgia';
-                        $result = pdo($pdo, $sql,);
-                        if ($result->rowCount() > 0) {
-                            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+        <div class="row">
+            <div class="col-sm">
+                <h3 class="text-center text-uppercase fw-bold">Thêm mới tác giả</h3>
+                <form action="add_author.php" method="POST">
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatId">Mã tác giả</span>
+                        <input type="text" class="form-control" name="txtCatId">
+                    </div>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tên tác giả</span>
+                        <input type="text" class="form-control" name="txtCatName">
+                    </div>
 
-                        ?>
-                                <tr>
-                                    <th scope="row"><?php echo $row['ma_tgia'] ?></th>
-                                    <td><?php echo $row['ten_tgia'] ?></td>
-                                    <td><?php echo $row['hinh_tgia'] ?></td>
-                                    <td>
-                                        <a href="edit_author.php?id=1"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    </td>
-                                    <td>
-                                        <a href=""><i class="fa-solid fa-trash"></i></a>
-                                    </td>
-                                </tr>
-                        <?php
-                            }
-                        }; ?>
-                    </tbody>
-                </table>
+                    <!-- <form action="upload.php" method="POST" enctype="multipart/form-data">
+                        <h6>Ảnh tác giả</h6>
+                        <input type="file" name="image">
+                        <input type="submit" value="Upload">
+                    </form> -->
+                    <div class="form-group  float-end ">
+
+                        <input type="submit" value="Thêm" class="btn btn-success">
+                        <a href="author.php" class="btn btn-warning ">Quay lại</a>
+                    </div>
+
+                </form>
             </div>
         </div>
-
-
-
-
+        <?php
+        // Lấy giá trị từ form
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $txtCatId = $_POST["txtCatId"];
+            $txtCatName = $_POST["txtCatName"];
+            $sql = "INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (:cardid, :cardname)";
+            $stmt = pdo($pdo, $sql, ['cardid' => $txtCatId, 'cardname' => $txtCatName]);
+        }
+        ?>
     </main>
     <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
--- codeWeb/admin/edit_author.php
+++ codeWeb/admin/edit_author.php
@@ -1,7 +1,3 @@
-<?php
-require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +5,7 @@ require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Author</title>
+    <title>Music for Life</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
@@ -34,10 +30,10 @@ require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
                             <a class="nav-link" href="../index.php">Trang ngoài</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="category.php">Thể loại</a>
+                            <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active fw-bold" href="author.php">Tác giả</a>
+                            <a class="nav-link" href="author.php">Tác giả</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="article.php">Bài viết</a>
@@ -46,56 +42,35 @@ require '/xampp/htdocs/CSE485_2023/codeWeb/connect.php';
                 </div>
             </div>
         </nav>
+
     </header>
     <main class="container mt-5 mb-5">
-        <div>
-            <div>
-                <button type="button" class="btn btn-primary">
-                    <a href="add_author.php?id=1" style="text-decoration: none; color:aliceblue">Thêm mới</a>
-                </button>
-            </div>
-            <div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Mã tác giả</th>
-                            <th>Tên tác giả</th>
-                            <th>Hình ảnh tác giả</th>
-                            <th>Sửa</th>
-                            <th>Xóa</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $sql = 'select * from tacgia';
-                        $result = pdo($pdo, $sql,);
-                        if ($result->rowCount() > 0) {
-                            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+        <div class="row">
+            <div class="col-sm">
+                <h3 class="text-center text-uppercase fw-bold">Sửa thông tin Tác Giả</h3>
+                <form action="process_add_category.php" method="post">
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatId">Mã tác giả</span>
+                        <input type="text" class="form-control" name="txtCatId">
+                    </div>
 
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tên tác giả</span>
+                        <input type="text" class="form-control" name="txtCatName" >
+                    </div>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="image">
+                        <input type="submit" value="Upload">
+                    </form>
 
-                        ?>
-                                <tr>
-                                    <th scope="row"><?php echo $row['ma_tgia'] ?></th>
-                                    <td><?php echo $row['ten_tgia'] ?></td>
-                                    <td><?php echo $row['hinh_tgia'] ?></td>
-                                    <td>
-                                        <a href="edit_author.php?id=1"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    </td>
-                                    <td>
-                                        <a href=""><i class="fa-solid fa-trash"></i></a>
-                                    </td>
-                                </tr>
-                        <?php
-                            }
-                        }; ?>
-                    </tbody>
-                </table>
+                    <div class="form-group  float-end ">
+                        <input type="submit" value="Lưu lại" class="btn btn-success">
+                        <a href="category.php" class="btn btn-warning ">Quay lại</a>
+                    </div>
+                </form>
             </div>
         </div>
-
-
-
-
     </main>
     <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
