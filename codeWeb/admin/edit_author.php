00945c4d317700a6b2dd5f31837ac0be45f893d0
Long Nhật <doannhat802@gmail.com>
2023-02-27 23:12:47 +0700

    update all function author

--- codeWeb/admin/edit_author.php
+++ codeWeb/admin/edit_author.php
@@ -89,6 +89,7 @@ $result = pdo($pdo,$sql, ['id' => $id])->fetch();
             }
         }
         ?>
+        
     </main>
     <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
